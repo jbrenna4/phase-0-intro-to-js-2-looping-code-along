b67fcd9ffbbe2885ab50ae5250515914187bd962
Jim Brennan <jim.j.brennan@gmail.com>
2022-12-05 17:52:50 -0500

    commit tree ae26c6ab0ac167dbaa784734a693dab80fc1b5ec

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Aki"];
+const eventName = "surprise"
+let message;
+
+
+function writeCards(names, eventName) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return newArray
+}
+
+console.log(writeCards(names, eventName))
+
+console.log(writeCards(["Billy", "Bob", "Chuck"], "Christmas"))
+
+
+let down = [11]
+function countDown(down) {
+    while (down >= 0) {
+    console.log(down);
+    down--;
+  }
+  return down;
+}
+
+countDown(down);
